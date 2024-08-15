13d03a459e09f6935f50d3d3e836110d6b07f3e4
Bert <bert.k@hotmail.com>
2024-08-15 08:11:02 +0200

    add benchmarks for coverlet.core and coverlet.msbuild

---
+++ test/coverlet.msbuild.benchmark.tests/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Toni Solarin-Sodara
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+
+namespace coverlet.msbuild.benchmark.tests
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var config = DefaultConfig.Instance
+             .WithOptions(ConfigOptions.JoinSummary)
+             .AddJob(Job
+               .ShortRun
+               .WithLaunchCount(1)
+               .WithToolchain(InProcessNoEmitToolchain.Instance));
+      var summary = BenchmarkRunner.Run(new[]{
+            BenchmarkConverter.TypeToBenchmarks( typeof(CoverageResultTaskBenchmarks), config),
+            //BenchmarkConverter.TypeToBenchmarks( typeof(InstrumentationTaskBenchmarks), config)
+            });
+
+      // Use this to select benchmarks from the console:
+      //var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+    }
+  }
+}
