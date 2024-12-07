b9e4f85839f33079b2e90dd356adb6a8d814a20b
Bert <bert.k@hotmail.com>
2024-12-07 10:21:32 +0100

    create coverlet.tests.projectsample.wpf8.tests

---
+++ test/coverlet.tests.projectsample.wpf8.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Toni Solarin-Sodara
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Windows;
+
+namespace coverlet.tests.projectsample.wpf8.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+      var myCheck = new issue_1713();
+      Assert.NotNull(myCheck);
+
+      Assert.Equal(MessageBoxButton.OK, myCheck.Method_1713());
+    }
+}
--- test/coverlet.tests.projectsample.wpf8/TestClass.cs
+++ test/coverlet.tests.projectsample.wpf8/TestClass.cs
@@ -1,12 +1,22 @@
 ﻿// Copyright (c) Toni Solarin-Sodara
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Windows;
 using System.Windows.Controls;
 
 namespace coverlet.tests.projectsample.wpf8
 {
-    public class TestClass
+  public class TestClass
+  {
+    public UserControl? Control { get; set; }
+  }
+
+  public class issue_1713
+
+  {
+    public MessageBoxButton Method_1713(MessageBoxButton button = MessageBoxButton.OK)
     {
-        public UserControl? Control { get; set; }
+      return button;
     }
+  }
 }
