70751e718b6b8cf45490384ffc4c5cce83af3c1e
Bert <bert.k@hotmail.com>
2025-05-31 08:18:50 +0200

    rework CommandLineOptions

--- src/coverlet.MTP/CommandLineOptions.cs
+++ src/coverlet.MTP/CommandLineOptions.cs
@@ -1,56 +1,54 @@
 ﻿// Copyright (c) Toni Solarin-Sodara
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-//using coverlet.MTP.Extension;
 using Microsoft.Testing.Platform.Extensions;
 using Microsoft.Testing.Platform.Extensions.CommandLine;
 
 namespace coverlet.MTP
 {
 
-  public sealed class CommandLineOption
+  internal class CommandLineOptionsProvider : ICommandLineOptionsProvider
   {
-    public CommandLineOption(string name, string description, ArgumentArity arity, bool isHidden)
+    private readonly IExtension _extension;
+
+    public CommandLineOptionsProvider(IExtension extension)
     {
-      Name = name;
-      Description = description;
-      Arity = arity;
-      IsHidden = isHidden;
+      _extension = extension;
     }
 
-    public string Name { get; }
-    public string Description { get; }
-    public ArgumentArity Arity { get; }
-    public bool IsHidden { get; }
-  }
+    public Task<bool> IsEnabledAsync()
+    {
+      return _extension.IsEnabledAsync();
+    }
 
-  public class CommandLineOptionsProvider
-  {
-    private readonly List<CommandLineOption> _commandLineOptions = new()
-      {
-        new CommandLineOption(name: "formats", description: "Specifies the output formats for the coverage report (e.g., 'json', 'lcov').", arity: ArgumentArity.OneOrMore, isHidden: false),
-        new CommandLineOption(name: "exclude", description: "Filter expressions to exclude specific modules and types.", arity: ArgumentArity.OneOrMore, isHidden: false),
-        new CommandLineOption(name: "include", description: "Filter expressions to include only specific modules and type", arity: ArgumentArity.OneOrMore, isHidden: false),
-        new CommandLineOption(name: "exclude-by-file", description: "Glob patterns specifying source files to exclude.", arity: ArgumentArity.OneOrMore, isHidden: false),
-        new CommandLineOption(name: "include-directory", description: "Include directories containing additional assemblies to be instrumented.", arity: ArgumentArity.OneOrMore, isHidden: false),
-        new CommandLineOption(name: "exclude-by-attribute", description: "Attributes to exclude from code coverage.", arity: ArgumentArity.OneOrMore, isHidden: false),
-        new CommandLineOption(name: "include-test-assembly", description: "Specifies whether to report code coverage of the test assembly.", arity: ArgumentArity.Zero, isHidden: false),
-        new CommandLineOption(name: "single-hit", description: "Specifies whether to limit code coverage hit reporting to a single hit for each location", arity: ArgumentArity.Zero, isHidden: false),
-        new CommandLineOption(name: "skipautoprops", description: "Neither track nor record auto-implemented properties.", arity: ArgumentArity.Zero, isHidden: false),
-        new CommandLineOption(name: "does-not-return-attribute", description: "Attributes that mark methods that do not return", arity: ArgumentArity.ZeroOrMore, isHidden: false),
-        new CommandLineOption(name: "exclude-assemblies-without-sources", description: "Specifies behavior of heuristic to ignore assemblies with missing source documents.", arity: ArgumentArity.ZeroOrOne, isHidden: false),
-        new CommandLineOption(name: "source-mapping-file", description: "Specifies the path to a SourceRootsMappings file.", arity: ArgumentArity.ZeroOrOne, isHidden: false),
+    public string Uid => _extension.Uid;
 
-      };
+    public string Version => _extension.Version;
 
-    public string Uid => "Coverlet.MTP.CommandLineOptionsProvider";
-    public string Version => "1.0.0";
-    public string DisplayName => "Coverlet MTP Command Line Options Provider";
-    public string Description => "Provides command line options for Coverlet MTP.";
-    public Task<bool> IsEnabledAsync() => Task.FromResult(true);
-    public IReadOnlyCollection<CommandLineOption> GetCommandLineOptions() => _commandLineOptions.AsReadOnly();
+    public string DisplayName => _extension.DisplayName;
 
-    public Task<ValidationResult> ValidateOptionArgumentsAsync(Microsoft.Testing.Platform.Extensions.CommandLine.CommandLineOption commandOption, string[] arguments)
+    public string Description => _extension.Description;
+
+    public IReadOnlyCollection<CommandLineOption> GetCommandLineOptions()
+    {
+      return
+          [
+              new CommandLineOption(name: "formats", description: "Specifies the output formats for the coverage report (e.g., 'json', 'lcov').", arity: ArgumentArity.OneOrMore, isHidden: false),
+              new CommandLineOption(name: "exclude", description: "Filter expressions to exclude specific modules and types.", arity: ArgumentArity.OneOrMore, isHidden: false),
+              new CommandLineOption(name: "include", description: "Filter expressions to include only specific modules and type", arity: ArgumentArity.OneOrMore, isHidden: false),
+              new CommandLineOption(name: "exclude-by-file", description: "Glob patterns specifying source files to exclude.", arity: ArgumentArity.OneOrMore, isHidden: false),
+              new CommandLineOption(name: "include-directory", description: "Include directories containing additional assemblies to be instrumented.", arity: ArgumentArity.OneOrMore, isHidden: false),
+              new CommandLineOption(name: "exclude-by-attribute", description: "Attributes to exclude from code coverage.", arity: ArgumentArity.OneOrMore, isHidden: false),
+              new CommandLineOption(name: "include-test-assembly", description: "Specifies whether to report code coverage of the test assembly.", arity: ArgumentArity.Zero, isHidden: false),
+              new CommandLineOption(name: "single-hit", description: "Specifies whether to limit code coverage hit reporting to a single hit for each location", arity: ArgumentArity.Zero, isHidden: false),
+              new CommandLineOption(name: "skipautoprops", description: "Neither track nor record auto-implemented properties.", arity: ArgumentArity.Zero, isHidden: false),
+              new CommandLineOption(name: "does-not-return-attribute", description: "Attributes that mark methods that do not return", arity: ArgumentArity.ZeroOrMore, isHidden: false),
+              new CommandLineOption(name: "exclude-assemblies-without-sources", description: "Specifies behavior of heuristic to ignore assemblies with missing source documents.", arity: ArgumentArity.ZeroOrOne, isHidden: false),
+              new CommandLineOption(name: "source-mapping-file", description: "Specifies the path to a SourceRootsMappings file.", arity: ArgumentArity.ZeroOrOne, isHidden: false)
+          ];
+    }
+
+    public Task<ValidationResult> ValidateOptionArgumentsAsync(CommandLineOption commandOption, string[] arguments)
     {
       if (commandOption.Name == "formats")
       {
@@ -78,10 +76,13 @@ namespace coverlet.MTP
           return Task.FromResult(ValidationResult.Invalid($"The value '{arguments[0]}' is not a valid option for '{commandOption.Name}'."));
         }
       }
+      return ValidationResult.ValidTask;
+    }
 
+    public Task<ValidationResult> ValidateCommandLineOptionsAsync(Microsoft.Testing.Platform.CommandLine.ICommandLineOptions commandLineOptions)
+    {
       return ValidationResult.ValidTask;
     }
 
   }
-
 }
--- src/coverlet.MTP/CoverletCollectorExtension.cs
+++ src/coverlet.MTP/CoverletCollectorExtension.cs
@@ -5,7 +5,6 @@
 // Coverlet instrumentation should be done before any test is executed, and the coverage data should be collected after all tests have run.
 // Coverlet collects code coverage data and does not need to be aware of the test framework being used. It also does not need test case details or test results.
 
-using coverlet.MTP.Extension;
 using Coverlet.Core;
 using Coverlet.Core.Abstractions;
 using Coverlet.Core.Enums;
@@ -13,7 +12,9 @@ using Coverlet.Core.Helpers;
 using Coverlet.Core.Reporters;
 using Coverlet.Core.Symbols;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Testing.Platform.Extensions.TestHost;
+using Microsoft.Testing.Platform.Builder;
+//using Microsoft.Testing.Platform.Extensions;
+//using Microsoft.Testing.Platform.Extensions.TestHost;
 using Microsoft.Testing.Platform.TestHost;
 
 namespace coverlet.MTP
@@ -21,46 +22,36 @@ namespace coverlet.MTP
   /// <summary>
   /// Implements test session lifetime handling for coverage collection using the Microsoft Testing Platform.
   /// </summary>
-  internal sealed class CoverletCollectorExtension : IExtension, ITestSessionLifetimeHandler
+  internal sealed class CoverletCollectorExtension
   {
     private readonly ILogger _logger;
     private readonly CoverletMTPConfiguration _configuration;
     private readonly IServiceProvider _serviceProvider;
     private Coverage? _coverage;
-    private string? _sessionId;
+    //private readonly string? _sessionId;
+    private readonly ITestApplicationBuilder _builder;
+
+    private readonly CoverletExtension _extension;
 
     /// <summary>
     /// Initializes a new instance of the CoverletCollectorExtension class.
     /// </summary>
-    public CoverletCollectorExtension(ILogger logger)
+    public CoverletCollectorExtension(ILogger logger, ITestApplicationBuilder builder)
     {
       _logger = logger ?? throw new ArgumentNullException(nameof(logger));
       _configuration = new CoverletMTPConfiguration();
       _serviceProvider = CreateServiceProvider();
+      _builder = builder;
+      _extension = new CoverletExtension();
     }
 
     /// <inheritdoc/>
-    public string Uid => "Coverlet.MTP";
-
-    /// <inheritdoc/>
-    public string Version => "1.0.0";
-
-    /// <inheritdoc/>
-    public string DisplayName => "Coverlet Code Coverage Collector";
-
-    /// <inheritdoc/>
-    public string Description => "Provides code coverage collection for the Microsoft Testing Platform";
-
-    /// <inheritdoc/>
-    public Task<bool> IsEnabledAsync() => Task.FromResult(true);
-
-    /// <inheritdoc/>
-    public async Task<bool> BeforeTestSessionStartAsync(TestSessionContext context)
+    public async Task BeforeRunAsync(CancellationToken cancellationToken)
     {
       try
       {
-        _sessionId = context.SessionUid.ToString();
-        _logger.LogInformation($"Initializing coverage collection for session {_sessionId}");
+        //_sessionId = context.SessionUid.ToString();
+        //_logger.LogInformation($"Initializing coverage collection for session {_sessionId}");
 
         var parameters = new CoverageParameters
         {
@@ -92,78 +83,75 @@ namespace coverlet.MTP
           _logger.LogInformation($"Code coverage instrumentation completed. Instrumented {prepareResult.Results.Length} modules");
         });
 
-        return true;
       }
       catch (Exception ex)
       {
         _logger.LogError("Failed to initialize code coverage");
         _logger.LogError(ex);
-        return false;
       }
     }
 
     /// <inheritdoc/>
-    public async Task<bool> AfterTestSessionEndAsync(TestSessionContext context)
+    public async Task AfterRunAsync(int exitCode, CancellationToken cancellation)
     {
       try
       {
         if (_coverage == null)
         {
           _logger.LogError("Coverage instance not initialized");
-          return false;
         }
-
-        _logger.LogInformation("\nCalculating coverage result...");
-        CoverageResult result = _coverage.GetCoverageResult();
-
-        string dOutput = _configuration.OutputDirectory != null ? _configuration.OutputDirectory : Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar.ToString();
-
-        string directory = Path.GetDirectoryName(dOutput)!;
-
-        if (!Directory.Exists(directory))
+        else
         {
-          Directory.CreateDirectory(directory);
+          _logger.LogInformation("\nCalculating coverage result...");
+          CoverageResult result = _coverage!.GetCoverageResult();
+
+          string dOutput = _configuration.OutputDirectory != null ? _configuration.OutputDirectory : Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar.ToString();
+
+          string directory = Path.GetDirectoryName(dOutput)!;
+
+          if (!Directory.Exists(directory))
+          {
+            Directory.CreateDirectory(directory);
+          }
+
+          ISourceRootTranslator sourceRootTranslator = _serviceProvider.GetRequiredService<ISourceRootTranslator>();
+          IFileSystem fileSystem = _serviceProvider.GetService<IFileSystem>()!;
+
+          // Convert to coverlet format
+          foreach (string format in _configuration.formats)
+          {
+            IReporter reporter = new ReporterFactory(format).CreateReporter();
+            if (reporter == null)
+            {
+              throw new InvalidOperationException($"Specified output format '{format}' is not supported");
+            }
+
+            if (reporter.OutputType == ReporterOutputType.Console)
+            {
+              // Output to console
+              _logger.LogInformation("  Outputting results to console", important: true);
+              _logger.LogInformation(reporter.Report(result, sourceRootTranslator), important: true);
+            }
+            else
+            {
+              // Output to file
+              string filename = Path.GetFileName(dOutput);
+              filename = (filename == string.Empty) ? $"coverage.{reporter.Extension}" : filename;
+              filename = Path.HasExtension(filename) ? filename : $"{filename}.{reporter.Extension}";
+
+              string report = Path.Combine(directory, filename);
+              _logger.LogInformation($"  Generating report '{report}'", important: true);
+              await Task.Run(() => fileSystem.WriteAllText(report, reporter.Report(result, sourceRootTranslator)));
+            }
+          }
+
+          _logger.LogInformation("Code coverage collection completed");
         }
-
-        ISourceRootTranslator sourceRootTranslator = _serviceProvider.GetRequiredService<ISourceRootTranslator>();
-        IFileSystem fileSystem = _serviceProvider.GetService<IFileSystem>()!;
-
-        // Convert to coverlet format
-        foreach (string format in _configuration.formats)
-        {
-          IReporter reporter = new ReporterFactory(format).CreateReporter();
-          if (reporter == null)
-          {
-            throw new InvalidOperationException($"Specified output format '{format}' is not supported");
-          }
-
-          if (reporter.OutputType == ReporterOutputType.Console)
-          {
-            // Output to console
-            _logger.LogInformation("  Outputting results to console", important: true);
-            _logger.LogInformation(reporter.Report(result, sourceRootTranslator), important: true);
-          }
-          else
-          {
-            // Output to file
-            string filename = Path.GetFileName(dOutput);
-            filename = (filename == string.Empty) ? $"coverage.{reporter.Extension}" : filename;
-            filename = Path.HasExtension(filename) ? filename : $"{filename}.{reporter.Extension}";
-
-            string report = Path.Combine(directory, filename);
-            _logger.LogInformation($"  Generating report '{report}'", important: true);
-            await Task.Run(() => fileSystem.WriteAllText(report, reporter.Report(result, sourceRootTranslator)));
-          }
-        }
-
-        _logger.LogInformation("Code coverage collection completed");
-        return true;
       }
       catch (Exception ex)
       {
         _logger.LogError("Failed to collect code coverage");
         _logger.LogError(ex);
-        return false;
       }
     }
 
